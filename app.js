24b0bb7670770d6117b31cfc565bb51873c9dec7
jesse soldat <jsoldat@hotmail.com>
2016-08-08 08:29:58 +0700

    can pass session info into the template

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+
 	http = require('http')
 	bodyParser   = require('body-parser'),
 	path = require('path');
@@ -8,11 +9,24 @@ var app = express();
 
 
 var port     = process.env.PORT || 3000;
-app.set('view', __dirname + '/');
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+var sessionInfo = {
+	name: 'Guest'
+};
+
+var session = function(req, res, next) {
+	req.sessionInfo = sessionInfo;
+	next();
+
+};
+
+app.get('/', session, function(req, res){
+	res.render('index.ejs', req.sessionInfo);
+});
 
 app.listen(port);
 console.log("Its alive! On " + port);
